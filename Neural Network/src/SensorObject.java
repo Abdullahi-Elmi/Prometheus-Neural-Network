dc2f6a4eabaa4eea129728af970c3811ec243d2c
Cullen O'Connor <cscullen4640@gmail.com>
2018-12-19 12:13:39 -0500

    This commit brings code in check with what was described in the project report pdf.

--- Neural Network/src/SensorObject.java
+++
@@ -1,7 +0,0 @@
-public interface SensorObject{
-    public Class getSignal();
-    public int[] getX();
-    public int[] getY();
-    public int getRange();
-    public double score(TheWorld world, int[][] visible);
-}
